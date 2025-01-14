86944d6c87ddd9e73cf7956075f645142ef369f4
Sam Matzko <matzko+sam@gmail.com>
2025-01-14 12:13:47 -0500

    Finished first draft LRV P3010 model.

--- lrv_p3010/init.lua
+++ lrv_p3010/init.lua
@@ -45,7 +45,7 @@ local train_def = {
         coupler_types_front = {tomlinson = true},
         assign_to_seat_group = {"driver_stand"},
         is_locomotive = true,
-        wagon_span = 3.1,
+        wagon_span = 3.600,
         wheel_positions = {1, -1},
         collisionbox = {
             -1.0, -0.5, -1.0,
